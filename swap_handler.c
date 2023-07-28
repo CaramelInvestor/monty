a06ad2ceff0113d203391187781f7314ddd2addd
CaramelInvestor <128856519+CaramelInvestor@users.noreply.github.com>
2023-07-28 07:31:40 +0100

    Update swap_handler.c

--- swap_handler.c
+++ swap_handler.c
@@ -4,7 +4,7 @@
  * @stack: ==========
  * @line_number: =========
  */
-void swap_handler(stack_t **stack, unsigned int line_number)
+void swap_handler(stack1_t **stack, unsigned int line_number)
 {
 	(void) stack;
 	(void) line_number;
