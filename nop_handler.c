980726d06bcd0d35b50de177a3c4edfee8748f28
CaramelInvestor <128856519+CaramelInvestor@users.noreply.github.com>
2023-07-28 07:28:43 +0100

    Update nop_handler.c

--- nop_handler.c
+++ nop_handler.c
@@ -4,7 +4,7 @@
  * @stack: ========
  * @line_number: =========
  */
-void nop_handler(stack_t **stack, unsigned int line_number)
+void nop_handler(stack1_t **stack, unsigned int line_number)
 {
 	(void) stack;
 	(void) line_number;
