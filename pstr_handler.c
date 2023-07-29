2cf68ec1ad30597265c0b1a20a1c2744075b1e07
caramelinvestor <caramelinvestor@gmail.com>
2023-07-29 05:36:23 -0700

    pstr

--- pstr_handler.c
+++ pstr_handler.c
@@ -1,11 +1,24 @@
 #include "monty.h"
 /**
  * pstr_handler - =========
- * @stack: =========
- * @line_number: ============
+ * @h: =========
+ * @ln: ============
  */
-void pstr_handler(stack1_t **stack, unsigned int line_number)
+void pstr_handler(stack1_t **h, unsigned int ln)
 {
-	(void) stack;
-	(void) line_number;
+	const stack1_t *curr = info.head2;
+
+	(void)h;
+	(void)ln;
+
+	while (curr != NULL)
+	{
+		if (curr->n <= 0 || curr->n > 127)
+			break;
+		printf("%c", (char)curr->n);
+		curr = curr->next;
+		/*i++;*/
+	}
+	printf("\n");
+	/*free_stack(h);*/
 }
