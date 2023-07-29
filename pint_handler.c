1e80484fd03968cda8d82abef533de5d6522ed5e
caramelinvestor <caramelinvestor@gmail.com>
2023-07-28 22:56:07 -0700

    pint

--- pint_handler.c
+++ pint_handler.c
@@ -7,6 +7,22 @@
 
 void pint_handler(stack1_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	const stack1_t *curr = info.head2;
+
+	(void)stack;
+	(void)line_number;
+	/*printtopstack(&head, line_number);*/
+	if (curr == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->n);
+		break;
+		/*curr = curr->next;*/
+		/*i++;*/
+	}
+	/*free_stack(h);*/
 }
--- testcase/00.m
+++ testcase/00.m
@@ -1,4 +1,4 @@
-pushe 1
+push 1
 push 2
 push 3
-pall
+pint
